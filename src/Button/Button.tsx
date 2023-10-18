b85e8605cff43309079a89c99cb4069f03a37c8b
ehsanghaffarii <ghafari.5000@gmail.com>
2023-10-18 16:50:17 +0330

    Add Button component with primary and secondary styles

---
+++ src/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Button.css";
+
+export interface ButtonProps extends React.ComponentProps<"button"> {
+ kind?: "primary" | "secondary";
+}
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+ ({ kind = "primary", ...props }, ref) => {
+   return <button data-button={`kind:${kind}`} ref={ref} {...props} />;
+ },
+);
