0c3661dcec7625430d53d1a020fd3e5a89bb5a2c
ehsanghaffarii <ghafari.5000@gmail.com>
2023-10-19 04:35:26 +0330

    Disable sourcemap in build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   build: {
     minify: false,
-    sourcemap: false,
+    // sourcemap: false,
     lib: { entry: 'src/index.ts', fileName: 'index', formats: ['es'] },
   },
 })
